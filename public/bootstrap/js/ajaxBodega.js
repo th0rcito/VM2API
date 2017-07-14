3b78a87ebe1e13d61546d8ec3fe081c1c27f23c3
th0rcito <javneira@gmail.com>
2017-07-14 18:13:23 -0400

    Add delete and getall

--- public/bootstrap/js/ajaxBodega.js
+++ public/bootstrap/js/ajaxBodega.js
@@ -9,7 +9,7 @@
 //      agregarRegistroDOM(data);
 //    }
 //   });
-// });
+// });api/bodega/delete/
 
 $(document ).ready(function() {
   $.ajax('http://localhost:3000/api/bodega/getAll', {
@@ -21,19 +21,39 @@ $(document ).ready(function() {
     agregarRegistroDOM(itemData);
     });
     $('#example').DataTable( );
-
+    agregarAccionEliminar();
   });
 
 });
 
+function agregarAccionEliminar(){
+ $(".eliminar").click(function(){
+   var bId = $(this).attr("data-id");
+   console.log(bId);
+   eliminarFromServer(bId);
+ });
+}
 
 
 function agregarRegistroDOM (oData) {
-  var oTr = '<tr id="tr_bodega' + oData.bod_id + '">' +
+  var oTr = '<tr id="tr_bodega_' + oData.bod_id + '">' +
               '<td id="td_bodega_id_' + oData.bod_id + '">' + oData.bod_id + '</td>' +
               '<td id="td_bodega_nombre_' + oData.bod_id + '">' + oData.bod_nombre +'</td>' +
               '<td id="td_bodega_direccion_' + oData.bod_id + '">' + oData.bod_direccion + '</td>' +
+              '<td><button type="button" class="btn btn-info" id="bodega_edit_'+oData.bod_id+'" data-id="'+oData.bod_id+'"><span class="glyphicon glyphicon-pencil"></span></button>'+
+                '&nbsp &nbsp &nbsp'+'<button type="button" class="btn btn-danger eliminar" id="bodega_delete_'+oData.bod_id+'" data-id="'+oData.bod_id+'"><span class="glyphicon glyphicon-trash"></span></button>'+
+              '</td>'+
             '</tr>';
   document.getElementById('tbody_bodega').innerHTML =  document.getElementById('tbody_bodega').innerHTML + oTr;
 
 }
+
+
+function eliminarFromServer (iId) {
+
+  $.ajax('http://localhost:3000/api/bodega/delete/' + iId, {
+    method: 'DELETE'
+  }).then(function(jsonRespuesta) {
+    document.getElementById('tr_bodega_' + iId).remove();
+  })
+  }
