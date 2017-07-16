c051e08309bb2755017f24a3c24c9e30d7c0163e
th0rcito <javneira@gmail.com>
2017-07-16 00:26:01 -0400

    new Bodega js (show registres, edit, delete)

--- public/bootstrap/js/ajaxBodega.js
+++ public/bootstrap/js/ajaxBodega.js
@@ -1,47 +1,38 @@
-
 $(document ).ready(function() {
-  $.ajax('http://localhost:3000/api/bodega/getAll', {
-    method: 'GET'
-  }).then(function(jsonRespuesta) {
-    jQuery.each(jsonRespuesta.data, function(index, itemData) {
-      console.log(itemData);
-
-    agregarRegistroDOM(itemData);
-    });
-    $('#example').DataTable( );
-    agregarAccionEliminar();
-  });
-
+  listarRegistros();
 });
 
-function agregarAccionEliminar(){
- $(".eliminar").click(function(){
-   var bId = $(this).attr("data-id");
-   console.log(bId);
-   eliminarFromServer(bId);
- });
+
+var listarRegistros = function(){
+  var table = $("#example").DataTable({
+    "destroy":true,
+    "ajax":{
+      "method":"GET",
+      "url":"http://localhost:3000/api/bodega/getAll"
+    },
+    "columns": [
+      { "data": "bod_id" },
+      { "data": "bod_nombre" },
+      { "data": "bod_direccion" },
+      {"defaultContent":"<button type='button' class='btn btn-info editar'><span class='glyphicon glyphicon-pencil'></span></button>         "+
+                      "<button type='button' class='btn btn-danger eliminar'><span class='glyphicon glyphicon-trash'></span></button>"
+      }
+    ]
+  });
+  registroEditar("#example tbody",table);
+  registroEliminar("#example tbody",table);
 }
 
-
-function agregarRegistroDOM (oData) {
-  var oTr = '<tr id="tr_bodega_' + oData.bod_id + '">' +
-              '<td id="td_bodega_id_' + oData.bod_id + '">' + oData.bod_id + '</td>' +
-              '<td id="td_bodega_nombre_' + oData.bod_id + '">' + oData.bod_nombre +'</td>' +
-              '<td id="td_bodega_direccion_' + oData.bod_id + '">' + oData.bod_direccion + '</td>' +
-              '<td><button type="button" class="btn btn-info" id="bodega_edit_'+oData.bod_id+'" data-id="'+oData.bod_id+'"><span class="glyphicon glyphicon-pencil"></span></button>'+
-                '&nbsp &nbsp &nbsp'+'<button type="button" class="btn btn-danger eliminar" id="bodega_delete_'+oData.bod_id+'" data-id="'+oData.bod_id+'"><span class="glyphicon glyphicon-trash"></span></button>'+
-              '</td>'+
-            '</tr>';
-  document.getElementById('tbody_bodega').innerHTML =  document.getElementById('tbody_bodega').innerHTML + oTr;
-
+var registroEditar = function(tbody, table){
+  $("tbody").on("click","button.editar",function(){
+    var data=table.row($(this).parents("tr")).data();
+    console.log(data);
+  });
 }
 
-
-function eliminarFromServer (iId) {
-
-  $.ajax('http://localhost:3000/api/bodega/delete/' + iId, {
-    method: 'DELETE'
-  }).then(function(jsonRespuesta) {
-    document.getElementById('tr_bodega_' + iId).remove();
-  })
-  }
+var registroEliminar = function(tbody, table){
+  $("tbody").on("click","button.eliminar",function(){
+    var data=table.row($(this).parents("tr")).data();
+    console.log(data);
+  });
+}
